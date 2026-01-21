a9d3cd23bd40b083fd64ecaef81e6f34ca9008af
renanmazza11 <renanmazzabatista@gmail.com>
2026-01-20 23:07:12 -0300

    exercicios modulo 1 rocket

--- src/index.js
+++ src/index.js
@@ -1,30 +1,96 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  
+  const {username} = request.headers;
+
+  const usernameExist = users.find(user => user.username === username);
+
+  if(!usernameExist){
+    return response.status(404).json({error: "Username not Exist!"});
+  }
+
+  request.user = usernameExist;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+
+    const {name, username}= request.body;
+
+    const usernameExist = users.some(users => users.username === username);
+
+    if(usernameExist){
+      return response.status(400).json({error: "Username Already exist!"})
+    }
+
+    const newUser = {
+
+      id: uuidv4(),
+      name,
+      username,
+      todos: []
+    }
+
+    users.push(newUser)
+
+    return response.status(201).json({message: "Usuário criado com sucesso!", user: newUser})
+
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+  const {user} = request;
+
+  return response.status(200).json(user.todos)
+  
 });
 
+
+
+
+
+
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+  const {title, deadline} = request.body
+
+  const {user} = request;
+
+  const newTodos = {
+
+    id: uuidv4(),
+    title,
+    done: "false",
+    deadline: new Date(deadline),
+    created_at: new Date()
+
+  }
+
+  user.todos.push(newTodos);
+
+  return response.status(200).json(user);
+
+
 });
+
+
+
+
+
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
